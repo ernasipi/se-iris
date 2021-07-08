89b5cd4675b4f5bb1b7326bd3165d0299433502b
ernasipi <ernad.sipic@gmail.com>
2021-07-08 15:46:11 +0200

    Update iris.js

--- iris.js
+++ iris.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     $(".panel").each(function( index ) {
-		if ($(this).children(".panelHeader").text() == "Project Scope") {
-			$(this).children(".panelContent").addClass("vertical-table");
-		}
+	if ($(this).children(".panelHeader").text() == "Project Scope") {
+		$(this).children(".panelContent").addClass("vertical-table");
+	}
     });
 });
