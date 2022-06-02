c717a8cd1a95cb0e23fab4fc58e855967f240e15
ernasipi <ernad.sipic@gmail.com>
2022-06-02 10:47:05 +0200

    Update iris.js

--- iris.js
+++ iris.js
@@ -1,19 +1,40 @@
+var EpicHeadings = [];
+var EpicData = [];
+
 $( document ).ready(function() {
 	$(".panel").each(function() {
-		var header = $(this).children(".panelHeader").text();
-		var innerHeader = $(this).children("H1").text();
+		let header = $(this).children(".panelHeader").text();
 		if (header == "Project Scope" || header == "Project organizatioon") {
 			$(this).children(".panelContent").addClass("vertical-table");
-		} 
-		
-		if (innerHeader == "Deliveries") {
-			$(this).children(".panelContent").addClass("delivery-tables");
 		}
 	});
 });
 
+
+
+function updateEpicData() {
+	$(".panel .aui td").each(function() {
+		var content = $(this).text().trim();
+		if (content.substring(0,3) == "TI-") {
+			
+			let epicData = findEpicData(content);
+			console.log(epicData);
+		}
+	});
+}
+
+function findEpicData(key) {
+	EpicData.forEach(function (index) {
+		if (index["key"] == key) {
+			
+		}
+		return index;
+	});
+	return null;
+}
+
 function formatDate(date) {
-	var d = new Date(date),
+	let d = new Date(date),
 		month = '' + (d.getMonth() + 1),
 		day = '' + d.getDate(),
 		year = d.getFullYear();
@@ -27,43 +48,85 @@ function formatDate(date) {
 }
 
 function parseDate(str) {
-	var m = str.trim().match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+	let m = str.trim().match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
 	return (m) ? formatDate(new Date(m[3], m[2]-1, m[1])) : null;
 }
 
 function onElementInserted(containerSelector, elementSelector, callback) {
 
-	var onMutationsObserved = function(mutations) {
+	let onMutationsObserved = function(mutations) {
 		mutations.forEach(function(mutation) {
 			if (mutation.addedNodes.length) {
-				var elements = $(mutation.addedNodes).find(elementSelector);
-				for (var i = 0, len = elements.length; i < len; i++) {
+				let elements = $(mutation.addedNodes).find(elementSelector);
+				for (let i = 0, len = elements.length; i < len; i++) {
 					callback(elements[i]);
 				}
 			}
 		});
 	};
 
-	var target = $(containerSelector)[0];
-	var config = { childList: true, subtree: true };
-	var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-	var observer = new MutationObserver(onMutationsObserved);    
+	let target = $(containerSelector)[0];
+	let config = { childList: true, subtree: true };
+	let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+	let observer = new MutationObserver(onMutationsObserved);    
 	observer.observe(target, config);
 }
 
 onElementInserted('body', '.jira-issues', function(element) {
 	$(element).find("td").each(function() {
-		var newDate = parseDate($(this).text());
+		let newDate = parseDate($(this).text());
 		if (newDate !== null) {
 			$(this).text(newDate);
 		}
 	});
-	var panel = $(element).closest(".panel");
-	if (panel.children(".panelHeader").text() == "Project organization") {
+	let panel = $(element).closest(".panel");
+	let header = panel.children(".panelHeader").text();
+	if (header == "Project organization") {
 		panel.find(".panelContent a").each(function(){
 			if ($(this).text().includes("siemensenergyag.sharepoint.com/sites/fsporderdeliveryteam/Lists/Bemanning")) {
 				$(this).text("Staffing in projects (Bemanningsfilen)");
 			}
 		});
+	} else if (header == "Technical information") {
+		let dataTable = panel.find("table.aui tbody tr");
+		$(dataTable).each(function() {
+			if ($(this).hasClass("rowNormal") || $(this).hasClass("rowAlternate")) {
+				let index = 0;
+				let dataRow = [];
+				$(this).children("td").each(function () {
+					if (EpicHeadings[index] !== undefined) {
+						dataRow[EpicHeadings[index]] = $(this).text().trim();
+					}
+					
+					index++;
+				});
+				EpicData.push(dataRow);
+			} else if ($(this).text() != "") {
+				let index = 0;
+				$(this).children("th").each(function () {
+					let title = $(this).text().toLowerCase();
+					
+					if (title == "key") { 
+						EpicHeadings[index] = "key"; 
+					} else if (title == "summary") { 
+						EpicHeadings[index] = "summary"; 
+					} else if (title == "assignee") { 
+						EpicHeadings[index] = "assignee"; 
+					} else if (title == "reporter") { 
+						EpicHeadings[index] = "reporter"; 
+					} else if (title == "status") { 
+						EpicHeadings[index] = "status"; 
+					} else if (title == "epic name") { 
+						EpicHeadings[index] = "epic name"; 
+					} else if (title == "b-nummer") {
+						EpicHeadings[index] = "b-nummer"; 
+					}
+					
+					index++;
+				});
+				
+			}
+		});
+		updateEpicData();
 	}
 });
